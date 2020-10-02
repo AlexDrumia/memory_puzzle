cb1eb11cd87d85cfbcec26124eb54337bba40d73
AlexDrumia <69897831+AlexDrumia@users.noreply.github.com>
2020-10-02 14:50:56 +0200

    Add reveal method to Board class

--- board.rb
+++ board.rb
@@ -16,9 +16,22 @@ class Board
 		end
 	end
 
+	def reveal
+		card = guessed_pos
+		if !card.face_up
+			card.reveal
+			card.face_value
+		end
+	end
+
 	def guessed_pos
+		pos = prompt_for_pos
+		@grid[pos.first][pos.last]
+	end
+
+	def prompt_for_pos
 		puts "Please enter the position of the card you'd like to flip (e.g., '2,3')"
-		pos = gets.chomp.split(',').map(&:to_i)
+		gets.chomp.split(',').map(&:to_i)
 	end
 
 	private
