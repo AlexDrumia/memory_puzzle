21dba57db21f966f380ddf84d4d43622c41af55f
AlexDrumia <69897831+AlexDrumia@users.noreply.github.com>
2020-10-02 14:02:52 +0200

    Add guessed_pos to Board class

--- board.rb
+++ board.rb
@@ -16,6 +16,11 @@ class Board
 		end
 	end
 
+	def guessed_pos
+		puts "Please enter the position of the card you'd like to flip (e.g., '2,3')"
+		pos = gets.chomp.split(',').map(&:to_i)
+	end
+
 	private
 	
 	def cards
